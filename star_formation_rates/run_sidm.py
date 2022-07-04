df70f9bfd1875ef30c1c43f53d820da042d29cae
Jay Baptista <jay.baptista@yale.edu>
2022-07-04 19:45:14 +0000

    backup

---
+++ star_formation_rates/run_sidm.py
@@ -0,0 +1,18 @@
+import os.path as path
+from jmb_utils import obtainSFR
+
+simulation_list = [
+    "../../../data/m12f_sidm1",
+    "../../../data/m12i_sidm1",
+    "../../../data/m12m_sidm1",
+]
+
+rs50_list = [
+    3.8, # m12f
+    4.7, # m12i
+    8.2  # m12m
+]
+
+for i, sim in enumerate(simulation_list):
+    df = obtainSFR(sim, 60, rmax=5*rs50_list[i])
+    df.to_hdf(f"sidm_sfr_{path.split(sim)[-1]}.hdf", key="w")
